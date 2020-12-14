84289c022e54d2e6c6c1b0b478f9f064e7b0bd85
withs <withs@withsky-server>
2020-12-14 15:17:07 +0900

    테스트

---
+++ test/src/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Test {
+	
+	final int number;
+	
+	Test() {
+		number = 20;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		
+		final int a;
+		
+		a = 30;
+		
+	}
+}
